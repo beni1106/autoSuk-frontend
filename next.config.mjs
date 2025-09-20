cb20fef12cd55003285f56e10d1c3753e2a2758d
beni1106 <revannovendriii3@gmail.com>
2025-09-20 18:36:33 +0700

    mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: "/(.*)",
-                headers: [
-                    {
-                        key: "Content-Security-Policy",
-                        value: "frame-ancestors 'self' https://basspreneur.com;",
-                    },
-                    {
-                        key: "Cache-Control",
-                        value: "public, max-age=3600",
-                    },
-                    {
-                        key: "X-Content-Type-Options",
-                        value: "nosniff",
-                    },
-                ],
-            },
-        ];
-    },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://basspreneur.com",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOW-FROM https://basspreneur.com',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600',
+          },
+        ],
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:slug',           // contoh: /cahyo
+        destination: '/kontak/:slug', // redirect ke /kontak/cahyo
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
