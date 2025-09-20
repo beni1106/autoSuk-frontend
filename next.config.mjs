e1494e65d4bb959a27265c518ca3acf4d5a42f71
beni1106 <revannovendriii3@gmail.com>
2025-09-20 12:33:35 +0700

    cache

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,14 @@ const nextConfig = {
                         key: "Content-Security-Policy",
                         value: "frame-ancestors 'self' https://basspreneur.com",
                     },
+                    {
+                        key: "Cache-Control",
+                        value: "public, max-age=3600",
+                    },
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff",
+                    },
                 ],
             },
         ];
