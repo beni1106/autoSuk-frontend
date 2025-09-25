8eecc71186a15cfb6dceb5838e8e6f9ca43a1f13
beni1106 <revannovendriii3@gmail.com>
2025-09-26 01:56:23 +0700

    aewaes

--- src/app/[domain]/page.js
+++ src/app/[domain]/page.js
@@ -6,18 +6,15 @@ import Cookies from "js-cookie";
 
 export default function PageDomain({ params }) {
     const router = useRouter();
-    const { domain } = params;
+    const { domain } = params; // slug dari path
 
     useEffect(() => {
         if (domain) {
-            // simpan domain ke cookie
+            // simpan slug ke cookie
             Cookies.set("domain", domain, { expires: 7, path: "/" });
 
-            // redirect ke home dengan query biar langsung kebaca
-            const t = setTimeout(() => {
-                router.replace(`/?domain=${domain}`);
-            }, 200); // kasih delay dikit biar cookie terset
-            return () => clearTimeout(t);
+            // redirect ke home dengan query
+            router.replace(`/?domain=${domain}`);
         }
     }, [domain, router]);
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
-import DebugPanel from "./components/DebugPanel";
+
 
 
 const montserrat = Montserrat({
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         <main>{children}</main>
         <Footer />
-        <DebugPanel /> {/* ✅ tampil di semua halaman */}
+
       </body>
     </html>
   );
