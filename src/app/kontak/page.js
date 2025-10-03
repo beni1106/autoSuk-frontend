d12b6706df7ed14332dc76d6293efa4d2c172d6e
beni1106 <revannovendriii3@gmail.com>
2025-10-03 19:51:38 +0700

    y3s

--- src/app/kontak/page.js
+++ src/app/kontak/page.js
@@ -1,5 +1,8 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
+
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { Phone } from "lucide-react";
